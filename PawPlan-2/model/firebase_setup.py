52c3629ba2e17c37ed52e841f6d04f7f7a16938c
Edward Seoj Aquino <202301386@iacademy.edu.ph>
2026-07-24 22:29:25 +0800

    Delete PawPlan-2 directory

--- PawPlan-2/model/firebase_setup.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-if not firebase_admin._apps:
-    cred = credentials.Certificate("./pawplan_account.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client(database_id="pawplan")
